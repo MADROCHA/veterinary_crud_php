340d7126b1e4ac0c6f9440d9eab47798fc88e11b
Ales <aleswebgit@gmail.com>
2022-03-18 14:13:03 +0100

    add all info of appointment in view, fix tailwind cdn

--- src/Views/appointmentsView.php
+++ src/Views/appointmentsView.php
@@ -3,7 +3,25 @@
     <h1>Hola</h1>
     <?php
         foreach ($data["appointments"] as $appointment) {
-            echo "<p>{$appointment->getName()}</p>";
+            // print id, name, species, breed, date, time, reason, description, person, phone, mail, created_at, updated_at
+            echo <<<TAG
+                <div class="appointment">
+                    <h2 class="text-4xl">{$appointment->getName()}</h2>
+                    <p>{$appointment->getSpecies()}</p>
+                    <p>{$appointment->getBreed()}</p>
+                    <p>{$appointment->getDate()}</p>
+                    <p>{$appointment->getTime()}</p>
+                    <p>{$appointment->getReason()}</p>
+                    <p>{$appointment->getDescription()}</p>
+                    <p>{$appointment->getPerson()}</p>
+                    <p>{$appointment->getPhone()}</p>
+                    <p>{$appointment->getMail()}</p>
+                    <p>{$appointment->getCreatedAt()}</p>
+                    <p>{$appointment->getUpdatedAt()}</p>
+                </div>
+            TAG;
+            // echo "<div class='appointment'>";
+            // echo "<h2>{$appointment->getName()}</h2>";
         }
     ?>
 </main>
