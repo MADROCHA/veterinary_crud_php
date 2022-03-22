38c658fe3e80efddc269c4155fbe30b375dc7fc3
Ales <aleswebgit@gmail.com>
2022-03-22 10:39:53 +0100

    fix don't put html if breed is empty

--- src/Views/appointmentsView.php
+++ src/Views/appointmentsView.php
@@ -1,44 +1,49 @@
 <?php require_once('src\Views\Components\layout.php') ?>
 <main class="p-5">
-    <h1>Citas animalillos</h1>
-    <?php
-        foreach ($data["appointments"] as $appointment) {
-            // print id, name, species, breed, date, time, reason, description, person, phone, mail, created_at, updated_at
-            echo <<<TAG
-              <div class="border-2 border-sky-400 max-w-2xl mx-auto my-16 rounded-3xl overflow-hidden px-6 py-4 hover:shadow-lg hover:shadow-sky-400/40">
-              <div class="flex justify-between">
-                <div class="bg-sky-400 -mt-4 -ml-6 px-6 py-4 rounded-br-3xl flex-1">
-                  <h2 class="text-4xl font-black">Muxi</h2>
-                  <p class="text-lg">
-                    <span>{$appointment->getName()}</span>
-                    - 
-                    <span>{$appointment->getBreed()}</span>
-                  </p>
-                </div>
-                <div class="flex flex-col items-end text-sm flex-1">
-                  <h4 class="text-lg">Cita</h4>
-                  <p>{$appointment->getDate()}</p>
-                  <p>{$appointment->getTime()}</p>
-                </div>
-              </div>
-              <div class="my-2">
-                <h3 class="text-xl font-black">{$appointment->getReason()}</h3>
-                <p class="hidden">
-                  {$appointment->getDescription()}
-                </p>
-              </div>
-              <div class="text-gray-500">
-                <p>Demanada: {$appointment->getCreatedAt()}</p>
-              </div>
-              <div class="flex flex-col justify-between text-gray-500 sm:flex-row mt-2">
-                <p>{$appointment->getPerson()}</p>
-                <p>{$appointment->getPhone()}</p>
-                <p>{$appointment->getMail()}</p>
-              </div>
-              </div>
-            TAG;
-        }
-    ?>
+    <h1 class="text-xl font-bold uppercase text-center">Citas animalillos</h1>
+    <div class="grid gap-4 justify-around my-5 lg:grid-cols-2">
+        <?php
+            foreach ($data["appointments"] as $appointment) {
+                $printBreed = "";
+                if ($appointment->getBreed() != "") {
+                    $printBreed = " - <span>{$appointment->getBreed()}</span>";
+                }
+                // print id, name, species, breed, date, time, reason, description, person, phone, mail, created_at, updated_at
+                echo <<<TAG
+                  <div class="border-2 border-sky-400 max-w-2xl rounded-3xl overflow-hidden px-6 py-4 hover:shadow-lg hover:shadow-sky-400/40 place-self-stretch">
+                  <div class="flex justify-between">
+                    <div class="bg-sky-400 -mt-4 -ml-6 px-6 py-4 rounded-br-3xl flex-1">
+                      <h2 class="text-4xl font-black">{$appointment->getName()}</h2>
+                      <p class="text-lg">
+                        <span>{$appointment->getSpecies()}</span>
+                        {$printBreed}
+                      </p>
+                    </div>
+                    <div class="flex flex-col items-end text-sm flex-1">
+                      <h4 class="text-lg">Cita</h4>
+                      <p>{$appointment->getDate()}</p>
+                      <p>{$appointment->getTime()}</p>
+                    </div>
+                  </div>
+                  <div class="my-2">
+                    <h3 class="text-xl font-black">{$appointment->getReason()}</h3>
+                    <p class="">
+                      {$appointment->getDescription()}
+                    </p>
+                  </div>
+                  <div class="text-gray-500">
+                    <p>Demanada: {$appointment->getCreatedAt()}</p>
+                  </div>
+                  <div class="flex flex-col justify-between text-gray-500 sm:flex-row mt-2 gap-x-8">
+                    <p>{$appointment->getPerson()}</p>
+                    <p>{$appointment->getPhone()}</p>
+                    <p>{$appointment->getMail()}</p>
+                  </div>
+                  </div>
+                TAG;
+            }
+        ?>
+    </div>
 </main>
 
 <?php require_once('src\Views\Components\footer.php') ?>
