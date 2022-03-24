7c5029e347092bb8da2afa49ba665c31cdbadb2a
Jessica Mejia <jmejiaos713@gmail.com>
2022-03-24 14:47:41 +0100

    delated header's height

--- src/Views/Components/header.php
+++ src/Views/Components/header.php
@@ -1,4 +1,4 @@
-<header class="bg-[#E3F09B] flex content-center justify-center h-20">
+<header class="bg-[#E3F09B] flex content-center justify-center">
     <a href="./">
         <img class="h-14 my-4" src="assets\img\logo.png" >
     </a>
