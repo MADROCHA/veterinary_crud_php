88091eca5e4f1181a4bb695fd420d59d32d9b411
Jessica Mejia <jmejiaos713@gmail.com>
2022-03-24 14:45:15 +0100

    changed footer's margin width and margin height and linked the vet pet's logo to the main page

--- src/Views/Components/header.php
+++ src/Views/Components/header.php
@@ -1,3 +1,5 @@
 <header class="bg-[#E3F09B] flex content-center justify-center h-20">
-<img class="h-16 my-2" src="assets\img\logo.png">
+    <a href="./">
+        <img class="h-14 my-4" src="assets\img\logo.png" >
+    </a>
 </header>
